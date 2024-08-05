fa7a8fbddb0432b201eea8fd84508cb9e70f23d6
AndreGomesN <gomes10andre@gmail.com>
2024-08-05 14:55:14 -0300

    feat: add activated state for timer

--- game/app/components/Timer.jsx
+++ game/app/components/Timer.jsx
@@ -1,16 +1,18 @@
 "use client"
 import { useEffect } from "react"
 
-function Timer({seconds, setSeconds, minutes, setMinutes}) {
+function Timer({seconds, setSeconds, minutes, setMinutes, activated}) {
     useEffect(()=>{
-        let timeoutSeconds = setInterval(()=>{setSeconds(prev=>prev+1)},1000)
-        if(seconds===60){
-            setMinutes(minutes+1)
-            setSeconds(0)
+        if(activated){
+            let timeoutSeconds = setInterval(()=>{setSeconds(prev=>prev+1)},1000)
+            if(seconds===60){
+                setMinutes(minutes+1)
+                setSeconds(0)
+            }
+            
+            return()=>{clearInterval(timeoutSeconds)}
         }
-        
-        return()=>{clearInterval(timeoutSeconds)}
-    },[seconds])
+    },[activated,seconds])
 
     return (
         <div className="bg-white text-black w-auto inline-block rounded-md px-6 py-1 text-xl">{(minutes<10?"0"+minutes:minutes)+":"+(seconds<10?"0"+seconds:seconds)}</div>
